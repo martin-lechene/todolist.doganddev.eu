c6841d1508bce3810268839ce454367e3cfbe4fe
Martin Lechêne <martin@lechene.be>
2022-07-02 18:09:16 +0200

    Update route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
