161c27cd5b16cbb936e7744193e6520ce51c1c25
Martin Lechêne <martin@lechene.be>
2022-07-02 16:19:08 +0200

    Add 'change-password' controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function changePassword()
+    {
+        return view('auth.passwords.email');
+    }
 }
