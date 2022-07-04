68f8e6a72abde735dbe1221bf7037af6e60150ca
Martin Lechêne <martin@lechene.be>
2022-07-04 22:07:31 +0200

    Update Models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+
+use App\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     use HasFactory;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Get the user that owns the task.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
